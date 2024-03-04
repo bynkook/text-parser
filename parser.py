ce95c052255974ac7e684c3b235b1ffa630417d8
bynkook <41982943+bynkook@users.noreply.github.com>
2024-03-04 20:54:52 +0900

    minor update
    return no empty line.  convert string to float or integer.

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
 def text_extractor(infile):
-    output = []
+    output, output_text = [], []
     for line in infile:
         # handle spaces
         line1 = ','.join(line.split())
@@ -7,7 +7,23 @@ def text_extractor(infile):
         line2 = list(map(str.strip, line1.split(',')))
         # remove empty strings from list
         line3 = [x for x in line2 if x]
-        output.append(line3)
+        if line3 != []:
+            output_text.append(line3)
+            line4 = string_to_number_in_list(line3)
+            output.append(line4)
+    return output, output_text
+
+def string_to_number_in_list(line):
+    output = []
+    for x in line:
+        try:
+            converted_value = float(x)
+            if converted_value.is_integer():
+                output.append(int(converted_value))
+            else:
+                output.append(converted_value)
+        except ValueError:
+            output.append(x)
     return output
 
 def text_writer(input, outfile):
@@ -15,5 +31,6 @@ def text_writer(input, outfile):
         outfile.write(' '.join(line)+'\n')
 
 with open('./textparser/test.dat','rt') as infile, open('./textparser/test.out','wt') as outfile:
-    output = text_extractor(infile)
-    text_writer(output,outfile)
+    output, output_text = text_extractor(infile)
+    print(output)
+    text_writer(output_text,outfile)
